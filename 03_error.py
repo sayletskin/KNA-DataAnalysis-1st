b8df6b094e59c62f733c0079e3b1f8016d463cac
sayletskin <sayletskin@gmail.com>
2026-07-15 14:07:53 +0900

    SyntaxError 학습

---
+++ 03_error.py
@@ -0,0 +1,8 @@
+print("===SyntaxError 만들기===")
+
+# print("온도) # 따옴표 line 3 print("온도)
+# print("진동" # 괄호 line 4 print("진동"
+
+print("온도", 75) # line 6,  NameError '온도' defined
+print("진동", 2.3) # line 7, SyntaxError '('가 닫히지 않음
+print("압력:", 4.0) # line 8, SyntaxError invalid syntax
